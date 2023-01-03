fb6c1c63a379d39bc6cf4ee3c04feee91f11947a
Nisaarullah <nisarullah012@gmail.com>
2023-01-03 22:57:07 +0500

    Footer Updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 
 
 function Footer() {
@@ -6,21 +6,23 @@ function Footer() {
     <section className=''>
       <div className='row cus-footer'>
         <div className='text-light'>
-          <h4>
-            LOGO
-          </h4>
+          <img src='Images/Logo-2.jpg' alt=''></img>
       </div>
 
         <div>
           <h4 className='text-light'>More Links</h4>
-          <a href='Privacy Policy
-            Terms & Conditions'>Privacy Policy <br/>
+          <a className='text-light' href='Privacy Policy Terms & Conditions'>Privacy Policy <br/>
             Terms & Conditions</a>
         </div>
         <div className='text-light'>
           <h4>
             Contact us
           </h4>
+          <h5>
+          KP, IT Park Abbottabad<br/>
+          contact@pixelio.tech<br/>
+          +92-311-6993741
+          </h5>
         </div>
         <div className='text-light'>
           <h4>
@@ -28,6 +30,11 @@ function Footer() {
           </h4>
         </div>
       </div>
+        <div className='text-secondary text-small custom-footer border-top'>
+        <h4>
+        ©  Pixel io Technologies 2022
+        </h4>
+        </div>
 
     </section>
 
